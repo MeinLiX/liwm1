9bfd30c44a39f414e77e478066ac2f926265bce0
MeinLiX <meinlix@ukr.net>
2023-01-30 19:17:12 +0200

    Create AppException.cs
    Add AppException (#2)

---
+++ api/Core/Application/Exceptions/AppException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace Application.Exceptions;
+
+public class AppException : Exception
+{
+    public HttpStatusCode StatusCode { get; init; }
+
+    public AppException() { }
+
+    public AppException(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
+
