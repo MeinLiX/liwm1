0f76737ff141181128514cfbb220540e0e12a222
Valentin <84251113+m1adow@users.noreply.github.com>
2023-02-22 21:47:03 +0200

    Add lobby modal for connecting (#67)

--- client/src/app/_modals/lobby-connect/lobby-connect.component.ts
+++ client/src/app/_modals/lobby-connect/lobby-connect.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { LobbyConnectMode } from 'src/app/_models/lobbyConnectMode';
 
 @Component({
   selector: 'app-lobby-connect',
@@ -7,7 +8,11 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./lobby-connect.component.css']
 })
 export class LobbyConnectComponent {
+  lobbyConnectMode?: LobbyConnectMode;
 
   constructor(public modalRef: BsModalRef) { }
 
+  connectToLobby() {
+
+  }
 }
