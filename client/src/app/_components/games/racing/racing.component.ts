47279f281ffc47f95984fc506321974613b02f73
Valentin <84251113+m1adow@users.noreply.github.com>
2023-03-14 21:19:52 +0200

    Add car class and car adding (#45)

--- client/src/app/_components/games/racing/racing.component.ts
+++ client/src/app/_components/games/racing/racing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Car } from 'src/app/_models/car';
 import { LobbyService } from 'src/app/_services/lobby.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { LobbyService } from 'src/app/_services/lobby.service';
 export class RacingComponent implements OnInit {
   canvas?: HTMLCanvasElement;
   ctx?: CanvasRenderingContext2D | null;
+  cars?: Car[];
 
   constructor(private lobbyService: LobbyService) { }
 
@@ -21,7 +23,10 @@ export class RacingComponent implements OnInit {
         next: lobby => {
           const countOfCars = lobby ? lobby.users.length : 1;
           for (let i = 0; i < countOfCars; i++) {
-            this.drawCar();
+            this.createCar();
+            if (this.cars && this.cars.length > 0) {
+              this.drawCar(this.cars[this.cars.length - 1]);
+            }
             this.drawRoad();
           }
         }
@@ -29,13 +34,31 @@ export class RacingComponent implements OnInit {
     }
   }
 
+  private createCar() {
+    if (this.cars && this.cars.length > 1) {
+      let isLastIndexEven = (this.cars.length - 1) % 2 === 0;
+      if (isLastIndexEven) {
+        let car = this.cars.length - 3 !== -1
+          ? this.cars[this.cars.length - 3]
+          : this.cars[0];
+        this.cars.push(new Car(car.x + (40 * 1.5), car.y));
+      } else {
+        let car = this.cars[this.cars.length - 3];
+        this.cars.push(new Car(car.x - (40 * 1.5), car.y));
+      }
+    } else {
+      let cars = [new Car(480, 600)];
+      this.cars = cars;
+    }
+  }
+
   private drawRoad(): void {
 
   }
 
-  private drawCar(): void {
+  private drawCar(car: Car): void {
     if (this.ctx) {
-      this.ctx.rect(140, 100, 25, 35);
+      this.ctx.rect(car.x, car.y, 40, 65);
       this.ctx.stroke();
     }
   }
---
+++ client/src/app/_models/car.ts
@@ -0,0 +1,13 @@
+export class Car {
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+    }
+}
