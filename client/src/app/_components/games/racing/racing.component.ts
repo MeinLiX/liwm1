f5b3a0787dad65bbc12bace75396b10570180f08
Valentin <84251113+m1adow@users.noreply.github.com>
2023-03-16 20:53:17 +0200

    Add start game counter (#67)

--- client/src/app/_components/games/racing/racing.component.ts
+++ client/src/app/_components/games/racing/racing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { delay } from 'rxjs';
 import { Car } from 'src/app/_models/car';
 import { LobbyService } from 'src/app/_services/lobby.service';
 
@@ -8,12 +9,15 @@ import { LobbyService } from 'src/app/_services/lobby.service';
   styleUrls: ['./racing.component.css']
 })
 export class RacingComponent implements OnInit {
+
   readonly carWidth = 40;
   readonly carHeight = 65;
 
   canvas?: HTMLCanvasElement;
   ctx?: CanvasRenderingContext2D | null;
   cars?: Car[];
+
+  isGamePlaying = false;
 
   constructor(private lobbyService: LobbyService) { }
 
@@ -22,18 +26,56 @@ export class RacingComponent implements OnInit {
     if (this.canvas) {
       this.ctx = this.canvas.getContext('2d');
 
-      this.lobbyService.lobby$.subscribe({
-        next: lobby => {
-          const countOfCars = lobby ? lobby.users.length : 1;
-          for (let i = 0; i < countOfCars; i++) {
-            this.createCar();
-            if (this.cars && this.cars.length > 0) {
-              this.drawCar(this.cars[i]);
-              this.drawRoad(this.cars[i]);
+      if (this.ctx) {
+        this.lobbyService.lobby$.subscribe({
+          next: lobby => {
+            const countOfCars = lobby ? lobby.users.length : 1;
+            for (let i = 0; i < countOfCars; i++) {
+              this.createCar();
+              if (this.cars && this.cars.length > 0) {
+                this.drawCar(this.cars[i]);
+                this.drawRoad(this.cars[i]);
+              }
             }
           }
+        });
+
+        this.ctx.font = '72px serif';
+        this.ctx.fillText('Tap to start', 355, 350);
+      }
+    }
+  }
+
+  async onClick() {
+    if (!this.isGamePlaying) {
+      this.isGamePlaying = true;
+
+      const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+      if (this.ctx && this.canvas) {
+        for (let counter = 3; counter > 0; counter--) {
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+          this.ctx.font = '72px serif';
+          this.ctx.fillText(counter.toString(), 485, 350);
+
+          await sleep(1000);
         }
-      });
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawCars();
+      }
+    } else {
+
+    }
+  }
+
+  private drawCars() {
+    if (this.cars) {
+      for (let i = 0; i < this.cars.length; i++) {
+        this.drawCar(this.cars[i]);
+        this.drawRoad(this.cars[i]);
+      }
     }
   }
 
