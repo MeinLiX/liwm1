a9b4fd7948fcf0e900205a7999af607791f8601f
Valentin <84251113+m1adow@users.noreply.github.com>
2023-04-15 19:42:20 +0300

    Implement sending cars to clients (#70)

--- api/Core/Application/Hubs/RacingGameHub.cs
+++ api/Core/Application/Hubs/RacingGameHub.cs
@@ -23,28 +23,59 @@ public class RacingGameHub : Hub
 
     public override async Task OnConnectedAsync()
     {
-        var user = await HubExtensions.GetCallerAsAppUserAsync(Context, Clients, this.userRepository);
-        if (user is null)
+        var userWithLobby = await GetUserWithLobbyAsync();
+        if (userWithLobby.Item1 is not null && userWithLobby.Item2 is not null)
         {
-            return;
-        }
-
-        var car = await this.racingCarRepository.GetRacingCarByRacerNameAsync(user.UserName);
-        if (car is null)
-        {
-            car = new RacingCar
+            var car = await this.racingCarRepository.GetRacingCarByRacerNameAsync(userWithLobby.Item1.UserName);
+            if (car is null)
             {
-                RacerName = user.UserName
-            };
-            await this.racingCarRepository.AddRacingCarAsync(car);
+                car = new RacingCar
+                {
+                    RacerName = userWithLobby.Item1.UserName
+                };
+                await this.racingCarRepository.AddRacingCarAsync(car);
+            }
+
+            await Clients.Caller.SendAsync(RacingGameHubMethodNameConstants.CarCreated, car);
+            await Clients.Group(userWithLobby.Item2.LobbyName).SendAsync(RacingGameHubMethodNameConstants.RecievedNewRacingCar, car);
+            await Groups.AddToGroupAsync(Context.ConnectionId, userWithLobby.Item2.LobbyName);
         }
-        await Clients.Caller.SendAsync(RacingGameHubMethodNameConstants.RecieveRacingCar, car);
-        //TODO: Add group creating | adding. Add sending new car to other players 
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        //TODO: Add car deleting and group leaving
+        var userWithLobby = await GetUserWithLobbyAsync();
+        if (userWithLobby.Item1 is not null && userWithLobby.Item2 is not null)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, userWithLobby.Item2.LobbyName);
+
+            var car = await this.racingCarRepository.GetRacingCarByRacerNameAsync(userWithLobby.Item1.UserName);
+            if (car is not null)
+            {
+                await this.racingCarRepository.DeleteRacingCarByIdAsync(car.Id);
+                await Clients.Caller.SendAsync(RacingGameHubMethodNameConstants.CarHasBeenDeleted);
+                await Clients.Group(userWithLobby.Item2.LobbyName).SendAsync(RacingGameHubMethodNameConstants.OtherCarWithIdHasBeenDeleted, car.Id);
+            }
+        }
+
         await base.OnDisconnectedAsync(exception);
+    }
+
+    private async Task<(AppUser?, Lobby?)> GetUserWithLobbyAsync()
+    {
+        var user = await HubExtensions.GetCallerAsAppUserAsync(Context, Clients, this.userRepository);
+        if (user is null)
+        {
+            return (null, null);
+        }
+
+        var lobby = await this.lobbyRepository.GetLobbyWithUserAsync(user);
+        if (lobby is null)
+        {
+            await Clients.Caller.SendAsync(LobbyHubMethodNameConstants.NoLobbyWithSuchUser);
+            return (null, null);
+        }
+
+        return (user, lobby);
     }
 }
--- api/Core/Domain/Models/Constants/RacingGameHubMethodNameConstants.cs
+++ api/Core/Domain/Models/Constants/RacingGameHubMethodNameConstants.cs
@@ -2,5 +2,8 @@ namespace Domain.Models.Constants;
 
 public static class RacingGameHubMethodNameConstants
 {
-    public const string RecieveRacingCar = "RecieveRacingCar";
+    public const string CarCreated = "CarCreated";
+    public const string RecievedNewRacingCar = "RecievedNewRacingCar";
+    public const string CarHasBeenDeleted = "CarHasBeenDeleted";
+    public const string OtherCarWithIdHasBeenDeleted = "OtherCarWithIdHasBeenDeleted";
 }
